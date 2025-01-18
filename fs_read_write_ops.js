351874588ecc0cfb6f250bd12c38a855b22fc30b
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-01-18 18:48:17 -0500

    added createReadStream that filters for search term and writes to a file

--- fs_read_write_ops.js
+++ fs_read_write_ops.js
@@ -3,6 +3,9 @@ const csv = require("csv-parser");
 
 // check if file exists
 const inputFile = "input_countries.csv";
+let canTxt = "Canada";
+let usaTxt = "United States";
+let canOutput = "Canada.txt";
 let inputFileExists = fs.existsSync("input_countries.csv");
 let canTxtExists = fs.existsSync("canada.txt");
 let usaTxtExists = fs.existsSync("usa.txt");
@@ -31,14 +34,30 @@ if (usaTxtExists) {
     });
 }
 
+let filteredData = [];
 fs.createReadStream(inputFile)
     // piping data from input_countries.csv to CSV parser.
     .pipe(csv())
     // log each row
     .on("data", (row) => {
-        console.log(row);
+        // retrieves all values of current row as array
+        const rowValues = Object.values(row);
+        // check if value in current row contain the search term
+        if (rowValues.some((value) => value.includes(canTxt))) {
+            filteredData.push(row);
+        }
     })
     // log when csv file is finished processing
     .on("end", () => {
+        // Create CSV header by retrieving keys from the first object in input_countries.txt
+        // The keys are joined into string separated by commas and a newline character is appended.
+        const csvHeader = Object.keys(filteredData[0]).join(",") + "\n";
+        // Maps each row object to a string joined by
+        // commas and combines all rows into a single string and separated by a newline
+        const csvRows = filteredData
+            .map((row) => Object.values(row).join(","))
+            .join("\n");
+        // Write CSV content including the header and filtered rows to Canada.txt
+        fs.writeFileSync(canOutput, csvHeader + csvRows);
         console.log("CSV file successfully proccessed");
     });
